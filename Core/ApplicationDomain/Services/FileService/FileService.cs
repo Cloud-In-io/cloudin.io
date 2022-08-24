654fdadea7fafc0448ec1e815858ad60e0252131
kalleby santos <kalleby_santos@hotmail.com>
2022-08-24 19:03:10 +0100

    📦 NEW: Creating File Service

---
+++ Core/ApplicationDomain/Services/FileService/FileService.cs
@@ -0,0 +1,42 @@
+using System.ComponentModel.DataAnnotations;
+using AutoMapper;
+using CloudIn.Core.ApplicationDomain.Contracts.Providers;
+using CloudIn.Core.ApplicationDomain.Contracts.Repositories;
+using CloudIn.Core.ApplicationDomain.Entities;
+using CloudIn.Core.ApplicationDomain.Services.FileService.Interfaces;
+
+namespace CloudIn.Core.ApplicationDomain.Services.FileService;
+
+public class FileService : IFileService
+{
+    private readonly IMapper _mapper;
+    private readonly IFileSystemProvider _fileSystemProvider;
+    private readonly IFileRepository _fileRepository;
+    private readonly IFolderRepository _folderRepository;
+    private readonly IUserRepository _userRepository;
+
+    public FileService(
+        IMapper mapper,
+        IFileSystemProvider fileSystemProvider,
+        IFileRepository fileRepository,
+        IFolderRepository folderRepository,
+        IUserRepository userRepository
+    )
+    {
+        _mapper = mapper;
+        _fileSystemProvider = fileSystemProvider;
+        _fileRepository = fileRepository;
+        _folderRepository = folderRepository;
+        _userRepository = userRepository;
+    }
+
+    public Task<FileEntity> CreateFileAsync(ICreateFilePayload createFilePayload)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<FileEntity> WriteFileContentAsync(ICreateFilePayload createFilePayload)
+    {
+        throw new NotImplementedException();
+    }
+}
