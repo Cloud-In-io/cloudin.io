8f5e2c15c09675bf8e3ad9a38b823d0504f872a1
kalleby santos <kalleby_santos@hotmail.com>
2022-10-19 14:21:55 +0100

    📦 WebApi: Adding filtering and sorting to File query

--- Core/WebApi/GraphQl/Schema/Queries/FileQuery.cs
+++ Core/WebApi/GraphQl/Schema/Queries/FileQuery.cs
@@ -6,7 +6,18 @@ namespace CloudIn.Core.WebApi.GraphQl.Schema.Queries;
 [ExtendObjectType(typeof(BaseQuery))]
 public class FileQuery
 {
+    [UseFirstOrDefault]
     [UseProjection]
+    [UseFiltering]
+    [UseSorting]
+    public IEnumerable<FileEntity> GetFile([Service] IFileRepository fileRepository)
+    {
+        return fileRepository.GetAll().AsQueryable();
+    }
+
+    [UseProjection]
+    [UseFiltering]
+    [UseSorting]
     public IEnumerable<FileEntity> GetFiles([Service] IFileRepository fileRepository)
     {
         return fileRepository.GetAll().AsQueryable();
