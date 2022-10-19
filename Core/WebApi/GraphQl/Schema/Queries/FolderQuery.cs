c20242ea3ae436654429eccda27546d509f13d2f
kalleby santos <kalleby_santos@hotmail.com>
2022-10-19 14:21:31 +0100

    📦 WebApi: Adding filtering and sorting to Folder query

--- Core/WebApi/GraphQl/Schema/Queries/FolderQuery.cs
+++ Core/WebApi/GraphQl/Schema/Queries/FolderQuery.cs
@@ -6,7 +6,18 @@ namespace CloudIn.Core.WebApi.GraphQl.Schema.Queries;
 [ExtendObjectType(typeof(BaseQuery))]
 public class FolderQuery
 {
+    [UseFirstOrDefault]
     [UseProjection]
+    [UseFiltering]
+    [UseSorting]
+    public IEnumerable<FolderEntity> GetFolder([Service] IFolderRepository folderRepository)
+    {
+        return folderRepository.GetAll().AsQueryable();
+    }
+
+    [UseProjection]
+    [UseFiltering]
+    [UseSorting]
     public IEnumerable<FolderEntity> GetFolders([Service] IFolderRepository folderRepository)
     {
         return folderRepository.GetAll().AsQueryable();
