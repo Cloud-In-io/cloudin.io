2a073fca49ea90c682bbc5d5ba575c7ad8aab835
Kalleby Santos <kalleby.santos@hotmail.com>
2022-12-20 13:44:20 +0000

    📦 NEW: Creating a test plugin

---
+++ Plugins/MyWebPlugin/MyPlugin.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using CloudIn.Core.Infrastructure.Contracts;
+
+namespace MyWebPlugin;
+
+public class MyPlugin : IPlugin
+{
+    public string Name { get; set; } = nameof(MyWebPlugin);
+
+    public MyPlugin(IServiceCollection services)
+    {
+        Console.WriteLine($"{Name} - Plugin Setup");
+    }
+
+    public void Configure(params object[] parameter)
+    {
+        var app = parameter.FirstOrDefault(p => p is IApplicationBuilder) as IApplicationBuilder;
+
+        if(app == null)
+        {
+            throw new ArgumentException($"{Name} Plugin requires an instance of {nameof(IApplicationBuilder)}");
+        }
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapGet("/hello", async context =>
+            {
+                await context.Response.WriteAsync("Hello from MyPlugin!");
+            });
+        });
+    }
+}
